3cb4ec25d245b9dcd95279b338a4ec1abd245381
Sonhoang <Sonhoang@RUAPC>
2025-09-26 20:43:43 +0700

    Add project files.

---
+++ test/index.js
@@ -0,0 +1,54 @@
+﻿class FoodRatings {
+    constructor(foods, cuisines, ratings) {
+        // Map để lưu trữ thông tin món ăn: food_name -> {cuisine, rating}
+        this.foodInfo = new Map();
+
+        // Map để lưu trữ danh sách món ăn theo cuisine: cuisine -> Array of food names
+        this.cuisineToFoods = new Map();
+
+        // Khởi tạo dữ liệu
+        for (let i = 0; i < foods.length; i++) {
+            const food = foods[i];
+            const cuisine = cuisines[i];
+            const rating = ratings[i];
+
+            // Lưu thông tin món ăn
+            this.foodInfo.set(food, { cuisine, rating });
+
+            // Nhóm món ăn theo cuisine
+            if (!this.cuisineToFoods.has(cuisine)) {
+                this.cuisineToFoods.set(cuisine, []);
+            }
+            this.cuisineToFoods.get(cuisine).push(food);
+        }
+    }
+
+    changeRating(food, newRating) {
+        const foodData = this.foodInfo.get(food);
+        const cuisine = foodData.cuisine;
+
+        // Cập nhật rating trong foodInfo
+        this.foodInfo.set(food, { cuisine, rating: newRating });
+    }
+
+    highestRated(cuisine) {
+        const cuisineFoods = this.cuisineToFoods.get(cuisine);
+
+        let maxRating = -1;
+        let bestFood = "";
+
+        // Duyệt qua tất cả món ăn trong cuisine này
+        for (const food of cuisineFoods) {
+            const currentRating = this.foodInfo.get(food).rating;
+
+            // Chọn món ăn có rating cao nhất, nếu bằng nhau thì chọn lexicographically smaller
+            if (currentRating > maxRating ||
+                (currentRating === maxRating && (bestFood === "" || food < bestFood))) {
+                maxRating = currentRating;
+                bestFood = food;
+            }
+        }
+
+        return bestFood;
+    }
+}
